b0c5bef8d4bdee95a5a92455954b50bf0326a07d
kinokokinn7 <kinokokinn77@gmail.com>
2022-08-16 11:37:53 +0900

    1章#10まで完了
    【Unity】ドラクエ風シンプル2DRPGの作り方！　#10 特定の領域でエンカウント

--- Simple2DTurnBaseRPG/Assets/Scripts/PlayerController.cs
+++ Simple2DTurnBaseRPG/Assets/Scripts/PlayerController.cs
@@ -5,10 +5,14 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
 
-    // 目標：playerのAnimationをコードから制御する
-    // InputX
-    // InputY
-    // IsMoving
+    // 目標：ランダムエンカウント
+    // ・歩き終わったときに必ず敵に会う（ログを出す）
+    // ・...一定確率で
+    // ・...特定の領域で一定確率で敵にあう:（Layerを使う）
+    // ・敵にあったらバトルシーンに行く（Panelを出す）
+
+    [SerializeField] LayerMask solidObjectsLayer;
+    [SerializeField] LayerMask encountLayer;
 
     Animator animator;
     bool isMoving;
@@ -40,11 +44,17 @@ public class PlayerController : MonoBehaviour
         animator.SetBool("IsMoving", isMoving);
     }
 
+    // 1マス徐々に近づける
     IEnumerator Move(Vector3 direction)
     {
         isMoving = true;
-
         Vector3 targetPos = transform.position + direction;
+        if (isWalkable(targetPos) == false)
+        {
+            isMoving = false;
+            yield break;
+        }
+
         // 現在とターゲットの位置が違う場合、近づけ続ける
         while ((targetPos - transform.position).sqrMagnitude > Mathf.Epsilon)
         {
@@ -56,5 +66,29 @@ public class PlayerController : MonoBehaviour
         transform.position = targetPos;
 
         isMoving = false;
+
+        // 敵にあうか調べる
+        CheckForEncounts();
+    }
+
+    void CheckForEncounts()
+    {
+        // 移動した地点に、敵がいるか判断する
+        if (Physics2D.OverlapCircle(transform.position, 0.2f, encountLayer))
+        {
+            if (Random.Range(0, 100) < 10)   // 0-99までの数字がランダムに選ばれて、その数字が50より小さかったら
+            {
+                Debug.Log("敵に遭遇");
+            }
+        }
+    }
+
+
+    // 今から移動するところに移動できるか判定する関数
+    bool isWalkable(Vector3 targetPos)
+    {
+        // targetPosを中心に円形のRayを作る：SolidObjectsLayerにぶつかったらtrueが返ってくる
+        // そのためfalseか否かで歩行可能か判定する
+        return Physics2D.OverlapCircle(targetPos, 0.2f, solidObjectsLayer) == false;
     }
 }
