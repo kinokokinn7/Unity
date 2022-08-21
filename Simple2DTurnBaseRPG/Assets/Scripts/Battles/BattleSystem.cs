4644c41b69ac765a90fcf220664883158c8c53fa
kinokokinn7 <kinokokinn77@gmail.com>
2022-08-21 21:26:56 +0900

    2章#14まで完了
    【Unity】ドラクエ風シンプル2DRPGの作り方！　#14 バトルの流れとクラス図の把握【シーズン2】
    まで完了

---
+++ Simple2DTurnBaseRPG/Assets/Scripts/Battles/BattleSystem.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class BattleSystem : MonoBehaviour
+{
+
+    enum State
+    {
+        Start,
+        ActionSelection,
+        MoveSelection,
+        RunTurns,
+        BattleOver,
+
+    }
+
+    State state;
+
+    public UnityAction OnBattleOver;
+
+    public void BattleStart()
+    {
+        state = State.Start;
+        Debug.Log("バトル開始");
+    }
+
+    public void BattleOver()
+    {
+        OnBattleOver?.Invoke();
+    }
+
+    private void Update()
+    {
+        switch (state)
+        {
+            case State.Start:
+                break;
+            case State.ActionSelection:
+                HandleActionSelection();
+                break;
+            case State.MoveSelection:
+                HandleMoveSelection();
+                break;
+            case State.BattleOver:
+                break;
+        }
+    }
+}
