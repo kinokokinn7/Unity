2d21470e386c7b2b3f7ec5e52927f0247109f525
kinokokinn7 <kinokokinn77@gmail.com>
2023-05-20 22:40:56 +0900

    2DRPGの作り方13　「Menu」スクリプトの更新まで完了
    2DRPGの作り方13　Unityでアイテム欄の取得スクリプト・アイテムインベントリを開発する
    「Menu」スクリプトの更新まで完了。

--- RPG/Assets/Scripts/Menu/Menu.cs
+++ RPG/Assets/Scripts/Menu/Menu.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class Menu : MonoBehaviour
 {
+    public MenuRoot FirstMenuRoot;
+    Stack<Menu> _menuRootStack;
+
     /// <summary>
     /// メニューを開いているか否かを表すフラグ。
     /// true: メニューが開かれている状態
@@ -11,19 +14,160 @@ public class Menu : MonoBehaviour
     public bool DoOpen { get => gameObject.activeSelf; }
 
     /// <summary>
-    /// メニューを開く。
+    /// メニューウィンドウを開きます。
     /// </summary>
-    public void Open()
+    public virtual void Open()
     {
         gameObject.SetActive(true);
+        StartCoroutine(UpdateWhenOpen());
     }
 
     /// <summary>
-    /// 初期状態ではメニューを閉じる。
+    /// 現在フォーカスされているメニューウィンドウを閉じます。
+    /// </summary>
+    public virtual void Close()
+    {
+        while (_menuRootStack.Count > 0)
+        {
+            _menuRootStack.Peek().IsActive = false;
+            _menuRootStack.Pop();
+        }
+    }
+
+    /// <summary>
+    /// 現在フォーカスされているメニューウィンドウ。
+    /// </summary>
+    public MenuRoot CurrentMenuObj
+    {
+        get => _menuRootStack.Peek();
+    }
+
+    /// <summary>
+    /// 現在フォーカスされているメニュー項目。
+    /// </summary>
+    public MenuItem CurrentItem
+    {
+        get
+        {
+            var top = _menuRootStack.Peek();
+            return top.CurrenItem;
+        }
+    }
+
+    /// <summary>
+    /// 初期設定。メニューを閉じた状態にします。
     /// </summary>
     private void Awake()
     {
         gameObject.SetActive(false);
     }
 
+    /// <summary>
+    /// メニューウィンドウが開かれた時の処理。
+    /// 他のメニューを非アクティブにして、
+    /// 開いたメニューをアクティブにします。
+    /// </summary>
+    /// <returns></returns>
+    IEnumerator UpdateWhenOpen()
+    {
+        // 他のメニューを非アクティブにして、
+        // 開いたメニューをアクティブにする
+        var menuRoots = GetComponentsInChildren<MenuRoot>();
+        foreach (var root in menuRoots)
+        {
+            root.IsActive = false;
+        }
+        _menuRootStack = new Stack<MenuRoot>();
+        _menuRootStack.Push(FirstMenuRoot);
+        FirstMenuRoot.IsActive = true;
+
+        yield return null;
+
+        // アクティブなメニューウィンドウの入力を受け付ける
+        while (_menuRootStack.Count > 0)
+        {
+            var current = _menuRootStack.Peek();
+
+            // 「↑」または「←」キー押下の場合：カーソル移動（すすむ）
+            if (Input.GetKeyDown(KeyCode.UpArrow) ||
+                Input.GetKeyDown(KeyCode.LeftArrow))
+            {
+                current.Index--;
+                ChangeMenuItem(current);
+            }
+            // 「↓」または「→」キー押下の場合：カーソル移動（もどる）
+            else if (Input.GetKeyDown(KeyCode.DownArrow) ||
+              Input.GetKeyDown(KeyCode.RightArrow))
+            {
+                current.Index++;
+                ChangeMenuItem(current);
+            }
+            // スペースキー押下の場合：決定
+            else if (Input.GetKeyDown(KeyCode.Space))
+            {
+                Decide(current);
+            }
+            // Xキー押下の場合：キャンセル
+            else if (Input.GetKeyDown(KeyCode.X))
+            {
+                Cancel(current);
+            }
+            yield return null;
+        }
+
+        // メニューを全て閉じたら処理終了
+        gameObject.SetActive(false);
+    }
+
+    /// <summary>
+    /// メニューの項目を変更します。
+    /// </summary>
+    /// <param name="menuRoot"></param>
+    protected virtual void ChangeMenuItem(MenuRoot menuRoot)
+    {
+    }
+
+    /// <summary>
+    /// メニューの項目選択を決定します。
+    /// </summary>
+    /// <param name="current"></param>
+    protected virtual void Decide(menuRoot current)
+    {
+        var item = current.CurrentItem;
+        switch (item.CurrentKind)
+        {
+            // 次のメニューに移動する場合
+            case MenuItem.Kind.NextMenu:
+                if (item.MoveTargetObj.MenuItems.Length > 0)
+                {
+                    item.MoveTargetObj.IsActive = true;
+                    _menuRootStack.Push(item.MoveTargetObj);
+                    ChangeMenuItem(_menuRootStack.Peek());
+                }
+                break;
+            // 前のメニューに戻る場合
+            case MenuItem.Kind.BackMenu:
+                current.IsActive = false;
+                _menuRootStack.Pop();
+                if (_menuRootStack.Count > 0)
+                {
+                    ChangeMenuItem(_menuRootStack.Peek());
+                }
+                break;
+            // イベントを発動する場合
+            case MenuItem.Kind.Event:
+                item.Callbacks.Invoke();
+                break;
+        }
+    }
+
+    protected virtual void Cancel(MenuRoot current)
+    {
+        current.IsActive = false;
+        _menuRootStack.Pop();
+        if (_menuRootStack.Count > 0)
+        {
+            ChangeMenuItem(_menuRootStack.Peek());
+        }
+    }
 }
