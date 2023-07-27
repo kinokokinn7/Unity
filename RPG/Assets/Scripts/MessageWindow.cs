ccde2d1509e4926979dff14333bbcefc857d3cca
kinokokinn7 <kinokokinn77@gmail.com>
2023-07-27 22:12:11 +0900

    2DRPGの作り方14　「パラメータ付きテキストについて」まで完了
    2DRPGの作り方14　「パラメータ付きテキストについて」まで完了

--- RPG/Assets/Scripts/MessageWindow.cs
+++ RPG/Assets/Scripts/MessageWindow.cs
@@ -7,12 +7,21 @@ using System;
 
 public class MessageWindow : MonoBehaviour
 {
+    public const string YES_NO_MENU_LINE_TEXT = "<YESNO>";
+
     public string Message = "";
     public float TextSpeedPerChar = 1000 / 10f;
     [Min(1)] public float SpeedUpRate = 3f;
     [Min(1)] public int MaxLineCount = 4;
 
+    /// <summary>
+    /// メッセージ表示が終了しているかを表すフラグ。
+    /// </summary>
     public bool IsEndMessage { get; private set; } = true;
+
+    public YesNoMenu YesNoMenu;
+    public string[] Params { get; set; }
+
     Transform TextRoot;
     Text TextTemplate;
 
@@ -30,6 +39,19 @@ public class MessageWindow : MonoBehaviour
         StopAllCoroutines();
         gameObject.SetActive(true);
         StartCoroutine(MessageAnimation());
+    }
+
+    /// <summary>
+    /// メッセージウィンドウを閉じます。
+    /// 「はい/いいえ」選択ウィンドウもあわせて閉じます。
+    /// </summary>
+    public void Close()
+    {
+        StopAllCoroutines();
+        Params = null;
+        IsEndMessage = true;
+        YesNoMenu.gameObject.SetActive(false);
+        gameObject.SetActive(false);
     }
 
     private IEnumerator MessageAnimation()
@@ -54,26 +76,77 @@ public class MessageWindow : MonoBehaviour
             lineText.text = "";
             textObjs.Add(lineText);
 
-            for (var i = 0; i < line.Length; i++)
+            // <YESNO>タグの場合
+            if (line == YES_NO_MENU_LINE_TEXT)
             {
-                lineText.text += line[i];
-                var waitTimePerChar = TextSpeedPerChar / (Input.anyKey ? SpeedUpRate : 1);
-                yield return new WaitForSeconds(waitTimePerChar);
+                // 「はい/いいえ」メニューを開く
+                YesNoMenu.Open();
+
+                yield return new WaitWhile(() => YesNoMenu.DoOpen);
+            }
+            else
+            {
+                for (var i = 0; i < line.Length; i++)
+                {
+                    if (line[i] == '#' && (i + 1) < line.Length)
+                    {
+                        // パラメータテキストの場合（「#1」や「#2」など）
+                        // NOTE:
+                        // テキストに埋め込む際は「#1」や「#2」など#を頭文字にした後に数字を一文字指定してください。
+                        // 指定した数字を添え字として「MessageWindow」コンポーネントの
+                        // 「Params」フィールドに設定したものへ変換するようにしています。
+                        if (char.IsDigit(line[i + 1]))
+                        {
+                            var index = line[i + 1] - '0';
+                            var paramText = (index < Params.Length) ? Params[index] : $"#{line[i + 1]}";
+
+                            foreach (var ch in paramText)
+                            {
+                                lineText.text += ch;
+                                float speed = TextSpeedPerChar / (Input.anyKey ? SpeedUpRate : 1);
+                                yield return new WaitForSeconds(speed);
+                            }
+                        }
+                        // 文字が"#"の場合
+                        // NOTE:
+                        // テキストの中に「##」と書くと文字列のエスケープシーケンス「\\」と同じ感じで「#」に変換されます。
+                        else if (line[i + 1] == '#')
+                        {
+                            lineText.text += '#';
+                            float speed = TextSpeedPerChar / (Input.anyKey ? SpeedUpRate : 1);
+                            yield return new WaitForSeconds(speed);
+                        }
+                        // それ以外の場合
+                        else
+                        {
+                            lineText.text += line[i + 1];
+                            float speed = TextSpeedPerChar / (Input.anyKey ? SpeedUpRate : 1);
+                            yield return new WaitForSeconds(speed);
+                        }
+                        i++;
+                    }
+                    else
+                    {
+                        lineText.text += line[i];
+                        float speed = TextSpeedPerChar / (Input.anyKey ? SpeedUpRate : 1);
+                        yield return new WaitForSeconds(speed);
+                    }
+                }
+            }
+
+            // 全行表示後、任意のキーが押されたらウィンドウを閉じる
+            yield return new WaitUntil(() => Input.anyKeyDown);
+            Params = null;
+            IsEndMessage = true;
+            gameObject.SetActive(false);
+        }
+
+        void DestroyLineText()
+        {
+            foreach (var text in TextRoot.GetComponentsInChildren<Text>()
+                .Where(_t => _t != TextTemplate))
+            {
+                UnityEngine.Object.Destroy(text.gameObject);
             }
         }
-
-        // 全行表示後、任意のキーが押されたらウィンドウを閉じる
-        yield return new WaitUntil(() => Input.anyKeyDown);
-        IsEndMessage = true;
-        gameObject.SetActive(false);
     }
-
-    void DestroyLineText()
-    {
-        foreach (var text in TextRoot.GetComponentsInChildren<Text>()
-            .Where(_t => _t != TextTemplate))
-        {
-            UnityEngine.Object.Destroy(text.gameObject);
-        }
-    }
-}
