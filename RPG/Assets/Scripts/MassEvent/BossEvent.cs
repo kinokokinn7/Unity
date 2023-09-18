fb543d9c56f18dbe670adc8cdf40652ce0f05901
kinokokinn7 <kinokokinn77@gmail.com>
2023-09-18 11:53:04 +0900

    2DRPGの作り方23　ボス戦イベントを作成してシンボルエンカウントさせよう
    実装完了。

---
+++ RPG/Assets/Scripts/MassEvent/BossEvent.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "MassEvent/Boss Event")]
+public class BossEvent : MassEvent
+{
+    public EnemyGroup EncounterEnemies;
+
+    public override void Exec(RPGSceneManager manager)
+    {
+        var pos = manager.MassEventPos;
+        var boss = manager.ActiveMap.GetCharacter(pos) as Boss;
+        if (boss == null) return;
+
+        manager.StartCoroutine(Battle(manager));
+    }
+
+    IEnumerator Battle(RPGSceneManager manager)
+    {
+        var pos = manager.MassEventPos;
+        var boss = manager.ActiveMap.GetCharacter(pos) as Boss;
+
+        var battleWindow = manager.BattleWindow;
+        battleWindow.SetUseEncounter(EncounterEnemies);
+        battleWindow.Open();
+
+        yield return new WaitWhile(() => battleWindow.DoOpen);
+
+        if (manager.Player.BattleParameter.HP <= 0)
+        {
+            Debug.Log("Fail Boss Battle...");
+        }
+        else
+        {
+            boss.Kill();
+        }
+    }
+}
