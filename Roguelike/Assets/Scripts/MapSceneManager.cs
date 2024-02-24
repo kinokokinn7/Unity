6c9140a9013c6a453c2654c2dd9a79fb14d19f91
kinokokinn7 <kinokokinn77@gmail.com>
2024-02-24 22:42:42 +0900

    fix: 一部クラスにDocを追記

--- Roguelike/Assets/Scripts/MapSceneManager.cs
+++ Roguelike/Assets/Scripts/MapSceneManager.cs
@@ -3,11 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 using System.Linq;
 
+/// <summary>
+/// マップのシーン管理を担当するクラスです。
+/// マップの生成、ゲームオーバー画面の表示、セーブデータの復元などを行います。
+/// </summary>
 public class MapSceneManager : MonoBehaviour
 {
-    public GameObject GameOver;
-    public bool IsAutoGenerate = true;
-    [SerializeField] Map.GenerateParam GenerateParam;
+    public GameObject GameOver; // ゲームオーバー画面
+    public bool IsAutoGenerate = true; // 自動でマップを生成するかどうか
+    [SerializeField] Map.GenerateParam GenerateParam; // マップ生成のパラメータ
+
+    /// <summary>
+    /// マップを生成します。既存のマップを破棄し、新しいマップを生成パラメータに基づいて作成します。
+    /// </summary>
     public void GenerateMap()
     {
         var map = GetComponent<Map>();
@@ -17,13 +25,18 @@ public class MapSceneManager : MonoBehaviour
 
 
     [TextArea(3, 15)]
-    public string mapData =
+    public string mapData = // デフォルトのマップデータ
         "000P\n" +
         "0+++\n" +
         "0000\n" +
         "+++0\n" +
         "G000\n";
 
+    /// <summary>
+    /// オブジェクトが生成された際に呼び出されます。
+    /// ゲームオーバー画面を非表示にし、セーブデータがあればそれを用いてマップを復元します。
+    /// なければ新しいマップを生成します。
+    /// </summary>
     void Awake()
     {
         GameOver.SetActive(false);
@@ -60,7 +73,7 @@ public class MapSceneManager : MonoBehaviour
         }
     }
 
-    // デバッグ用のコード
+    // デバッグ用: スペースキーを押すとマップを再生成します。
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space))
